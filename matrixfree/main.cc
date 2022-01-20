171fe66206873fbdb2fe686163c951b919c4b1e3
cucui <cuic3@mail2.sysu.edu.cn>
2022-01-20 21:21:41 +0100

    play with matrix free

---
+++ matrixfree/main.cc
@@ -0,0 +1,98 @@
+//
+// Created by Cu Cui on 2022/1/20.
+//
+
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_values.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/sparse_matrix.h>
+
+#include <iostream>
+
+using namespace dealii;
+
+template <int dim, int fe_degree>
+void
+test_reference()
+{
+  Triangulation<dim>        triangulation;
+  DoFHandler<dim>           dof_handler(triangulation);
+  FE_Q<dim>                 fe(fe_degree);
+  AffineConstraints<double> constraints;
+  SparseMatrix<double>      system_matrix;
+  SparsityPattern           sparsity_pattern;
+  Vector<double>            solution;
+  Vector<double>            system_rhs;
+
+
+  // setup
+  GridGenerator::hyper_cube(triangulation);
+  triangulation.refine_global(1);
+  dof_handler.distribute_dofs(fe);
+  solution.reinit(dof_handler.n_dofs());
+  system_rhs.reinit(dof_handler.n_dofs());
+  DynamicSparsityPattern dsp(dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler,
+                                  dsp,
+                                  constraints,
+                                  /*keep_constrained_dofs = */ false);
+  sparsity_pattern.copy_from(dsp);
+  system_matrix.reinit(sparsity_pattern);
+
+  // assemble
+  const QGauss<dim>  quadrature_formula(fe.degree + 1);
+  FEValues<dim>      fe_values(fe,
+                          quadrature_formula,
+                          update_values | update_gradients |
+                            update_quadrature_points | update_JxW_values);
+  const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
+  FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
+  Vector<double>     cell_rhs(dofs_per_cell);
+  std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
+  for (const auto &cell : dof_handler.active_cell_iterators())
+    {
+      cell_matrix = 0;
+      cell_rhs    = 0;
+
+      fe_values.reinit(cell);
+      for (const unsigned int q_index : fe_values.quadrature_point_indices())
+        {
+          for (const unsigned int i : fe_values.dof_indices())
+            {
+              for (const unsigned int j : fe_values.dof_indices())
+                cell_matrix(i, j) +=
+                  (fe_values.shape_grad(i, q_index) * // grad phi_i(x_q)
+                   fe_values.shape_grad(j, q_index) * // grad phi_j(x_q)
+                   fe_values.JxW(q_index));           // dx
+
+              cell_rhs(i) += (1. *                                // f(x)
+                              fe_values.shape_value(i, q_index) * // phi_i(x_q)
+                              fe_values.JxW(q_index));            // dx
+            }
+        }
+      cell->get_dof_indices(local_dof_indices);
+      constraints.distribute_local_to_global(
+        cell_matrix, cell_rhs, local_dof_indices, system_matrix, system_rhs);
+    }
+
+  // output
+  system_matrix.print_formatted(std::cout);
+  system_rhs.print(std::cout);
+}
+
+int
+main()
+{
+  test_reference<2, 1>();
+  return 0;
+}
